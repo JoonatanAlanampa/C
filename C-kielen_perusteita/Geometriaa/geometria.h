bdb653457dd643c18ff788cb7e0b15ef3d1d6d69
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 14:44:10 +0200

    Create geometria.h

---
+++ C-kielen_perusteita/Geometriaa/geometria.h
@@ -0,0 +1,10 @@
+#ifndef GEOMETRIA_H_INCLUDED
+#define GEOMETRIA_H_INCLUDED
+
+const long double PII;
+
+double ympyranAla(double sade);
+
+double pallonTilavuus(double sade);
+
+#endif /*GEOMETRIA_H_INCLUDED*/
