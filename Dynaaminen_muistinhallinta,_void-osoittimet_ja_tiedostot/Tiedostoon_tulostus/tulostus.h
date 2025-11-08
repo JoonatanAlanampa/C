b8d1e4d1632cd55f62c2ed1773505f8c84994523
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 15:18:00 +0200

    Create tulostus.h

---
+++ Dynaaminen_muistinhallinta,_void-osoittimet_ja_tiedostot/Tiedostoon_tulostus/tulostus.h
@@ -0,0 +1,6 @@
+#ifndef TULOSTUS_H_INCLUDED
+#define TULOSTUS_H_INCLUDED
+
+void tulostaDblTaulu(double *taulu, size_t lkm, const char *taulNimi, int tarkkuus, const char *tiedNimi);
+
+#endif /* TULOSTUS_H_INCLUDED */
