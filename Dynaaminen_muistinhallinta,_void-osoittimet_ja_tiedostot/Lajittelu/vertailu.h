54968603746c70eeb486ee34bd212c9adb3f4f3f
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 15:15:29 +0200

    Create vertailu.h

---
+++ Dynaaminen_muistinhallinta,_void-osoittimet_ja_tiedostot/Lajittelu/vertailu.h
@@ -0,0 +1,12 @@
+#ifndef VERTAILU_H_INCLUDED
+#define VERTAILU_H_INCLUDED
+
+int rectAlaVrt(const void *a, const void *b);
+
+int rectXyVrt(const void *a, const void *b);
+
+int rectLeveysVrt(const void *a, const void *b);
+
+int mjPitAakkosVrt(const void *a, const void *b);
+
+#endif /* VERTAILU_H_INCLUDED */
