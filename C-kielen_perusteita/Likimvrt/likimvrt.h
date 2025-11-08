f792a8bf3c7a2ab42e262933d5d6d1d162d0157e
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 14:49:49 +0200

    Create likimvrt.h

---
+++ C-kielen_perusteita/Likimvrt/likimvrt.h
@@ -0,0 +1,10 @@
+#ifndef LIKIMVRT_H_INCLUDED
+#define LIKIMVRT_H_INCLUDED
+
+double lueToleranssi();
+
+void asetaToleranssi(double uusiToleranssi);
+
+int doubleVrt(double a, double b);
+
+#endif /* LIKIMVRT_H_INCLUDED */
