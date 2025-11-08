600b34c8906396783c27aa3987224177c5ba772e
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 14:48:36 +0200

    Create unsigned.h

---
+++ C-kielen_perusteita/Etumerkittömät/unsigned.h
@@ -0,0 +1,12 @@
+#ifndef UNSIGNED_H_INCLUDED
+#define UNSIGNED_H_INCLUDED
+
+void ucharIntervalli(unsigned char sade, unsigned char askel);
+
+void ushortIntervalli(unsigned short sade, unsigned short askel);
+
+void uintIntervalli(unsigned int sade, unsigned int askel);
+
+void ulongIntervalli(unsigned long sade, unsigned long askel);
+
+#endif /* UNSIGNED_H_INCLUDED */
