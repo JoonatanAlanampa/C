3853fbbdb61ba6bb8da279565512f334ac1c91ff
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 14:50:06 +0200

    Create likimvrt.c

---
+++ C-kielen_perusteita/Likimvrt/likimvrt.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include "likimvrt.h"
+
+static double toleranssi = 0.000001;
+
+double lueToleranssi()
+{
+    return toleranssi;
+}
+
+void asetaToleranssi(double uusiToleranssi)
+{
+    toleranssi = uusiToleranssi;
+}
+
+int doubleVrt(double a, double b)
+{
+    if(fabs(a-b)<=toleranssi)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
