295141724682209d5407eb6f9803f2265d84ec9c
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 15:06:05 +0200

    Create kolmijako.h

---
+++ Bitit,_osoittimet_ja_tietueet/Kolmijako/kolmijako.h
@@ -0,0 +1,14 @@
+#ifndef KOLMIJAKO_H_INCLUDED
+#define KOLMIJAKO_H_INCLUDED
+
+struct IntPari
+{
+    int x;
+    int y;
+};
+
+typedef struct IntPari IntPari;
+
+IntPari kolmijako(unsigned int n, int t[], int vipu1, int vipu2, int *p1, int *p2);
+
+#endif /* KOLMIJAKO_H_INCLUDED */
